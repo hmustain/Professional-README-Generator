e6211a528c52be95e00f52a48c1be801897bb422
hmustain <hunter@bunkerbranding.com>
2022-12-08 09:27:40 -0600

    added validation formula to each input field that check to make sure user is inputting information

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
+// prev lesson 20 and youtube video https://www.youtube.com/watch?v=2VUQABoFOqw
 // const questions = [];
 inquirer
     .prompt([
@@ -11,11 +12,13 @@ inquirer
          type: 'input',
         name: 'project name',
         message: 'What is the name of your project?',
+        validate: (data)=>{ if(data){return true} else {return 'You must enter information to continue'}}
       },
       {
         type: 'input',
         name: 'description',
         message: 'Please enter a description about your project',
+        validate: (data)=>{ if(data){return true} else {return 'You must enter information to continue'}}
       },
       {
         type: 'list',
@@ -27,66 +30,73 @@ inquirer
         type: 'input',
         name: 'email',
         message: 'What is your email address?',
+        validate: (data)=>{ if(data){return true} else {return 'You must enter information to continue'}}
       },
       {
         type: 'input',
         name: 'github',
         message: 'What is your GitHub user name?',
+        validate: (data)=>{ if(data){return true} else {return 'You must enter information to continue'}}
       },
       {
         type: 'input',
         name: 'github url',
         message: 'What is the link to your GitHub account',
+        validate: (data)=>{ if(data){return true} else {return 'You must enter information to continue'}}
       },
       {
         type: 'input',
         name: 'linkedin',
         message: 'What is the link to your Linkedin account?',
+        validate: (data)=>{ if(data){return true} else {return 'You must enter information to continue'}}
       },
       {
         type: 'input',
         name: 'installation',
         message: 'What are the instructions for installation of the application?',
+        validate: (data)=>{ if(data){return true} else {return 'You must enter information to continue'}}
       },
       {
         type: 'input',
         name: 'usage',
         message: 'What are the instructions for usage? (either type out or paste a link to instructions)',
+        validate: (data)=>{ if(data){return true} else {return 'You must enter information to continue'}}
       },
       {
         type: 'input',
         name: 'credits',
         message: 'Who do you credit for this project?',
+        validate: (data)=>{ if(data){return true} else {return 'You must enter information to continue'}}
       },
     ])
 
 // Template for README.md
-const template = `# ${title}
+// const template = `# ${title}
 
-* [description](#description)
-* [license](#license)
-* [email](#email)
-* [github](#github)
-* [github url](#github url)
-* [linkedin](#linkedin)
-* [installation](#installation)
-* [usage](#usage)
-* [credits](#credits)
-## Description
-${description}
-## Installation
-${installation}
-## Usage
-${usage}
-## Credits
-${credits}
-## License
-${license}
-## Contact
-* Email ${email}
-* Github ${github}
-* Github URL${githuburl}
-* Linkedin ${linkedin}`
+// * [description](#description)
+// * [license](#license)
+// * [email](#email)
+// * [github](#github)
+// * [github url](#github url)
+// * [linkedin](#linkedin)
+// * [installation](#installation)
+// * [usage](#usage)
+// * [credits](#credits)
+// ## Description
+// ${description}
+// ## Installation
+// ${installation}
+// ## Usage
+// ${usage}
+// ## Credits
+// ${credits}
+// ## License
+// ${license}
+// ## Contact
+// * Email ${email}
+// * Github ${github}
+// * Github URL${githuburl}
+// * Linkedin ${linkedin}`
 
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
