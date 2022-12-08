96c16b69896a5b9e2b19dc737c0ab1b843528286
hmustain <hunter@bunkerbranding.com>
2022-12-08 10:42:11 -0600

    added strings to the contact sections

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ ${license}
 Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
 
 ## Contact
-${contact}
+'If you would like to reach me or have any questions, a list for how to contact me is provided${contact}
 `
 
 inquirer
